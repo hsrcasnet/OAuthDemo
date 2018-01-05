0d18a018c3f4431fdb769d2a78d8a2bc6dada3a1
Thomas Galliker <thomas@bluewin.ch>
2018-01-05 23:54:49 +0100

    Cleanup

--- OAuthDemo/Global.asax.cs
+++ OAuthDemo/Global.asax.cs
@@ -14,10 +14,7 @@ namespace OAuthDemo
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
-            //GlobalConfiguration.Configure(WebApiConfig.Register);
-            //RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
     }
 }
