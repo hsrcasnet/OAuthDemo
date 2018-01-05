516fe70787df61549780d56016741582952a5930
Thomas Galliker <thomas@bluewin.ch>
2018-01-05 22:31:56 +0100

    Initial commit

---
+++ OAuthDemo/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace OAuthDemo.Controllers
+{
+    [RoutePrefix("api/hello")]
+    public class HelloController : ApiController
+    {
+        [HttpGet]
+        [Route("sayhello")]
+        public string SayHello()
+        {
+            return "hello";
+        }
+    }
+}
---
+++ OAuthDemo/Scripts/index.js
@@ -0,0 +1,41 @@
+﻿
+
+$(function () {
+    var token = null;
+
+    $('#loginButton').click(function () {
+        var username = $('#username').val();
+        var password = $('#password').val();
+        console.log(username);
+        console.log(password);
+
+        // todo oauth login
+        $.ajax({
+            url: 'token',
+            data: "grant_type=password&username=" + user.userName + "&password=" + user.password,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            type: 'POST',
+            success: function(result) {
+                token = result;
+            },
+            error: function(request, status, error) {
+                console.log(request);
+                console.log(status);
+                console.log(error);
+            }
+        });
+    });
+
+    $('#callWebApiMethod').click(function() {
+        $.ajax({
+            url: '/api/hello/sayhello',
+            headers: {
+                'Authorization' : 'Bearer lkadsfdsfalkjfdafdaljfdsalkjfdsa'
+            },
+            success: function(result) {
+                $('<div>').text(result).appendTo($('#output'));
+            }
+        });
+    });
+
+});
